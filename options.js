e7629aeba4aa2db861d8ba46259827e281ac1457
cptapplesnapple <123199708+cptapplesnapple@users.noreply.github.com>
2023-06-01 01:28:10 -0400

    Enchantment Shit

---
+++ options.js
@@ -0,0 +1,27 @@
+var availableEnchants = ['Element 1', 'Element 2', 'Element 3'];
+var availableLevels = [1,2,3,4,5];
+var selectedEnchants = {};
+
+function addOption() {
+  // Add option logic
+}
+
+function removeOption() {
+  // Remove option logic
+}
+
+function generateOptionsHTML() {
+  // Generate options HTML logic
+}
+
+function isElementSelected(element) {
+  // Check if element is selected logic
+}
+
+function updateOptions() {
+  // Update options logic
+}
+
+function getSelectedElements() {
+  // Get selected elements logic
+}
