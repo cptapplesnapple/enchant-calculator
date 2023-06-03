1966b64f10b680534426f81730dc63ad089be9de
cptapplesnapple <123199708+cptapplesnapple@users.noreply.github.com>
2023-06-03 04:08:28 -0400

    I'm gonna kill myself

--- options.js
+++ options.js
@@ -1,43 +1,43 @@
-var availableEnchants = [
-  'Aqua Affinity',
-  'Bane of Arthropods',
-  'Blast Protection',
-  'Channeling',
-  'Curse of Binding',
-  'Curse of Vanishing',
-  'Depth Strider',
-  'Done',
-  'Efficiency',
-  'Feather Falling',
-  'Fire Aspect',
-  'Fire Protection',
-  'Flame',
-  'Fortune',
-  'Frost Walker',
-  'Impaling',
-  'Infinity',
-  'Knockback',
-  'Looting',
-  'Loyalty',
-  'Luck of the Sea',
-  'Lure',
-  'Mending',
-  'Multishot',
-  'Piercing',
-  'Power',
-  'Projectile Protection',
-  'Protection',
-  'Punch',
-  'Quick Charge',
-  'Respiration',
-  'Riptide',
-  'Sharpness',
-  'Silk Touch',
-  'Smite',
-  'Soul Speed',
-  'Sweeping Edge',
-  'Thorns',
-  'Unbreaking'
+var allEnchants = [
+    'Aqua Affinity',
+    'Bane of Arthropods',
+    'Blast Protection',
+    'Channeling',
+    'Curse of Binding',
+    'Curse of Vanishing',
+    'Depth Strider',
+    'Done',
+    'Efficiency',
+    'Feather Falling',
+    'Fire Aspect',
+    'Fire Protection',
+    'Flame',
+    'Fortune',
+    'Frost Walker',
+    'Impaling',
+    'Infinity',
+    'Knockback',
+    'Looting',
+    'Loyalty',
+    'Luck of the Sea',
+    'Lure',
+    'Mending',
+    'Multishot',
+    'Piercing',
+    'Power',
+    'Projectile Protection',
+    'Protection',
+    'Punch',
+    'Quick Charge',
+    'Respiration',
+    'Riptide',
+    'Sharpness',
+    'Silk Touch',
+    'Smite',
+    'Soul Speed',
+    'Sweeping Edge',
+    'Thorns',
+    'Unbreaking'
 ];
 var availableLevels = [1,2,3,4,5];
 var selectedEnchants = {};
@@ -45,19 +45,4 @@ var selectedEnchants = {};
 function toggleDropdown() {
     var dropdown = document.querySelector('.dropdown');
     dropdown.classList.toggle('open');
-}
-
-var enchantmentsList = document.getElementById('enchantments-list');
-    availableEnchants.forEach(function(enchantment) {
-      var li = document.createElement('li');
-      li.textContent = enchantment;
-      li.onclick = function() {
-        selectEnchantment(this);
-      };
-      enchantmentsList.appendChild(li);
-    });
-
-function selectEnchantment(element) {
-  var selectedEnchantment = element.textContent;
-  console.log("Selected enchantment: " + selectedEnchantment);
 }
